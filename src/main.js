07763620c50fc1fe1068f6fa307990179b38ed20
Daiane <adm.daianesilva@gmail.com>
2019-10-23 23:17:35 -0300

    async await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-alert('opa');
+const MinhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 200);
+  });
+
+MinhaPromise().then((response) => {
+  console.log(response);
+});
+
+async function executePromise() {
+  const response = await MinhaPromise();
+  console.log(response);
+}
+
+executePromise();
