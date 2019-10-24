1f9aaccdeae23e66fa16cb5eb576209a58ca1840
Daiane <adm.daianesilva@gmail.com>
2019-10-23 21:21:22 -0300

    exercicios1 e funções

--- exercicio1.js
+++ exercicio1.js
@@ -62,16 +62,16 @@ const usuarios = [
   { nome: "Lucas", idade: 30, empresa: "Facebook" }
 ];
 
-// const ArrayIdades = usuarios.map(({ idade }) => idade);
-// console.log(ArrayIdades);
-// const maiorDeIdade = usuarios.filter(({ idade }) => idade >= 18);
-// console.log(maiorDeIdade);
-// const google = usuarios.find(({ empresa }) => empresa === "Google");
-// console.log(google);
+const ArrayIdades = usuarios.map(({ idade }) => idade);
+console.log(ArrayIdades);
+const maiorDeIdade = usuarios.filter(({ idade }) => idade >= 18);
+console.log(maiorDeIdade);
+const google = usuarios.find(({ empresa }) => empresa === "Google");
+console.log(google);
 
 console.log(
   usuarios
-    .map(usuario => ({ ...usuario, idade: usuario.idade * 2 }))
+    .map((usuario) => ({ ...usuario, idade: usuario.idade * 2 }))
     .filter(({ idade }) => idade <= 50)
 );
 
@@ -81,7 +81,7 @@ arr.map(item > item + 10);
 // 3.2
 // Dica: Utilize uma constante pra function
 const usuario = { nome: "Diego", idade: 23 };
-const mostraIdade = usuario => usuario.idade;
+const mostraIdade = (usuario) => usuario.idade;
 mostraIdade(usuario);
 // 3.3
 // Dica: Utilize uma constante pra function
@@ -92,3 +92,42 @@ mostraUsuario(nome, idade);
 mostraUsuario(nome);
 // 3.4
 const promise = () => new Promise((resolve, reject) => resolve());
+
+const empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
+  }
+};
+
+const {
+  nome,
+  endereco: { cidade, estado }
+} = empresa;
+console.log(nome); // Rocketseat
+console.log(cidade); // Rio do Sul
+console.log(estado); // SC
+
+function mostraInfo({ nome, idade }) {
+  return `${nome} tem ${idade} anos.`;
+}
+
+console.log(mostraInfo({ nome: "Diego", idade: 23 }));
+
+const arr = [1, 2, 3, 4, 5, 6];
+const [x, ...y] = arr;
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
+
+const usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    uf: "SC",
+    pais: "Brasil"
+  }
+};
+
+const usuario2 = { ...usuario, nome: "Daiane" };
